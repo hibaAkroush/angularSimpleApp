566cad128e0378b08288cd8fd32a6e72f3c6f5fa
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-01-18 11:10:12 +0200

    sidebar

--- components/classified.ctrl.js
+++ components/classified.ctrl.js
@@ -2,11 +2,14 @@
 
 (function () {
 	angular.module("myApp")
-		.controller("myCtrl", function($scope, $http, myFactory){
+		.controller("myCtrl", function($scope, $http, myFactory, $mdSidenav){
 			$scope.message = "hello world"
 			myFactory.getData().then(function(res){
 					$scope.classifieds = res.data
 					})
+			$scope.openSidebar = function(){
+				$mdSidenav('left').open()
+			}
 
 		})
 })()

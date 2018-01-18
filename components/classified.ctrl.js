864088ee20ba36a3ae6d19d8c0240cdc06c933f5
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-01-18 12:38:24 +0200

    delete, edit and filter

--- components/classified.ctrl.js
+++ components/classified.ctrl.js
@@ -4,12 +4,42 @@
 	angular.module("myApp")
 		.controller("myCtrl", function($scope, $http, myFactory, $mdSidenav){
 			$scope.message = "hello world"
+			var contact= { 
+				"name":"John Doe",
+      			"phone":"(555) 555-5555",
+      			"email":"johndoe@gmail.com"
+			}
 			myFactory.getData().then(function(res){
 					$scope.classifieds = res.data
 					})
 			$scope.openSidebar = function(){
 				$mdSidenav('left').open()
 			}
+			$scope.closeSidebar = function(){
+				console.log("	close clocked")
+				$mdSidenav('left').close()
+			}
+			$scope.editClassified = function(classified){
+				console.log(classified)
+				$scope.openSidebar()
+				$scope.editing = true
+				$scope.classified = classified
+			}
 
+			$scope.saveClassified = function(classified){
+				if(classified.title != null || classified.price != null)
+				classified.contact = contact
+				$scope.classifieds.push(classified)
+			}
+			$scope.saveEdit = function(){
+				$scope.closeSidebar()
+				$scope.editing = false
+				$scope.classified = {}
+			}
+			$scope.delete = function(classified){
+				var iindex = $scope.classifieds.indexOf(classified)
+				if(confirm("are you sure?"))
+				$scope.classifieds.splice(iindex,1)
+			}
 		})
 })()

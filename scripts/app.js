05b6dd15ad3ff463c461d8daa4d18bcb9350aa92
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-01-18 15:06:02 +0200

    ui route

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,32 @@
-angular.module("myApp", ['ngMaterial'])
-		.config(function($mdThemingProvider){
-			$mdThemingProvider.theme('default')
-				.primaryPalette("teal")
-				.accentPalette('orange')
-		})
-		.directive("helloWorld", function () {
-			return {
-				template: "<h1>{{message}}</h1>"
-			}
-			
-		})
+var myApp = angular.module("myApp", ['ngMaterial', 'ui.router'])
+
+myApp.config(function($stateProvider, $mdThemingProvider){
+  console.log(":")
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+  
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+
+  $mdThemingProvider.theme('default')
+  .primaryPalette("teal")
+  .accentPalette('orange')
+})
+
+myApp.directive("helloWorld", function () {
+	return {
+		template: "<h1>{{message}}</h1>"
+	}
+})
+
+

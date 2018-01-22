bd264305e15e10eea3778ba4b2798771d34f1e00
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-01-22 15:49:43 +0200

    routes

--- scripts/app.js
+++ scripts/app.js
@@ -2,21 +2,27 @@ var myApp = angular.module("myApp", ['ngMaterial', 'ui.router'])
 
 myApp.config(function($stateProvider, $mdThemingProvider){
   console.log(":")
+  var home = {
+    name : 'home',
+    url : '/',
+    templateUrl : 'home.html'
+  }
   var helloState = {
     name: 'hello',
     url: '/hello',
-    template: '<h3>hello world!</h3>'
+    template: '<h3>page 1 template</h3>'
   }
   
   var aboutState = {
     name: 'about',
     url: '/about',
-    template: '<h3>Its the UI-Router hello world app!</h3>'
+    template: '<h3>page 2 template</h3>'
   }
 
 
   $stateProvider.state(helloState);
   $stateProvider.state(aboutState);
+  $stateProvider.state(home);
 
   $mdThemingProvider.theme('default')
   .primaryPalette("teal")
